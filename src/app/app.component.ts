bb17662314218fa9de02416a810491f47ae5dc78
Aniket kandrikar <75112852+aniket24122012@users.noreply.github.com>
2022-07-02 21:06:40 +0530

    ngModel or two way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular12';
+  
+  title = 'angular12 NgModel';
+
+  data:any;
 }
