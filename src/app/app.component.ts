8f9987f9201b715f5d931160b3a099cb0b849619
Aniket kandrikar <75112852+aniket24122012@users.noreply.github.com>
2022-07-03 11:13:01 +0530

    form template driven

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   
   title = 'angular12 NgModel';
-
-  data:any;
+  loginUser(item:any)
+  {
+    console.warn(item);
+  }
 }
